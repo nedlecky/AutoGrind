3cc576a7a93018a9ff6ef9721f816440cd192822
Ned Lecky <ned@lecky.com>
2022-04-16 06:40:36 -0500

    Class ordering on RTB extension to make VS happy Verison set to 2022.04.16.1

--- AutoGrind/MainForm.cs
+++ AutoGrind/MainForm.cs
@@ -25,23 +25,6 @@ using NLog;
 
 namespace AutoGrind
 {
-    public static class RichTextBoxExtensions
-    {
-        [DllImport("user32.dll")]
-        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
-        private const int WM_SETREDRAW = 0x0b;
-
-        public static void SuspendDrawing(this System.Windows.Forms.RichTextBox richTextBox)
-        {
-            SendMessage(richTextBox.Handle, WM_SETREDRAW, (IntPtr)0, IntPtr.Zero);
-        }
-
-        public static void ResumeDrawing(this System.Windows.Forms.RichTextBox richTextBox)
-        {
-            SendMessage(richTextBox.Handle, WM_SETREDRAW, (IntPtr)1, IntPtr.Zero);
-            richTextBox.Invalidate();
-        }
-    }
     public partial class MainForm : Form
     {
 
@@ -3395,5 +3378,22 @@ namespace AutoGrind
             RecipeRTBCopy.Text = RecipeRTB.Text;
         }
 
+    }
+    public static class RichTextBoxExtensions
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
+        private const int WM_SETREDRAW = 0x0b;
+
+        public static void SuspendDrawing(this System.Windows.Forms.RichTextBox richTextBox)
+        {
+            SendMessage(richTextBox.Handle, WM_SETREDRAW, (IntPtr)0, IntPtr.Zero);
+        }
+
+        public static void ResumeDrawing(this System.Windows.Forms.RichTextBox richTextBox)
+        {
+            SendMessage(richTextBox.Handle, WM_SETREDRAW, (IntPtr)1, IntPtr.Zero);
+            richTextBox.Invalidate();
+        }
     }
 }
