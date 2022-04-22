12ee3f92cd57fa9b2d98020135326ab561839ac4
Ned Lecky <ned@lecky.com>
2022-04-22 13:11:25 -0500

    Fixed robot_index to update at end of command in PolyScope. Also, it gets incremented in the stop thread whan a command is received to keep us in sync.

--- AutoGrind/Properties/AssemblyInfo.cs
+++ AutoGrind/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2022.04.22.1")]
-[assembly: AssemblyFileVersion("2022.04.22.1")]
+[assembly: AssemblyVersion("2022.04.22.2")]
+[assembly: AssemblyFileVersion("2022.04.22.2")]
