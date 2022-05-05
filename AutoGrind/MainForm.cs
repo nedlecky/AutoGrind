9566ff9f042f98ca0cb2be3e937eb036ea0bf2aa
Ned Lecky <ned@lecky.com>
2022-05-05 08:36:50 -0500

    Added debug messages for start/end of execution. First comes from robot_starting, and latter from robot_index

--- AutoGrind/MainForm.cs
+++ AutoGrind/MainForm.cs
@@ -2954,10 +2954,13 @@ namespace AutoGrind
                     RobotReadyLbl.BackColor = ColorFromBooleanName(valueTrimmed);
                     break;
                 case "robot_starting":
-                    log.Debug("robot starting {0}", valueTrimmed);
+                    // This gets sent to us by command_validate on the UR. It means command valueTrimmed is going to start executing
+                    log.Debug("UR<== STARTING {0}", valueTrimmed);
                     break;
                 case "robot_index":
+                    // This gets sent to us by PolyScope on the UR after command valueTrimmed has finished executing
                     RobotIndexLbl.Text = valueTrimmed;
+                    log.Debug("UR<== COMPLETED {0}", valueTrimmed);
 
                     // Color us green if we're caught up!
                     if (SentRobotIndexLbl.Text == RobotIndexLbl.Text)
