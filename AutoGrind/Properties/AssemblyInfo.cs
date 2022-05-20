06b3e970a787632aaa2731852b035e49178464e9
Ned Lecky <ned@lecky.com>
2022-05-20 07:23:27 -0700

    TQ Setup Notes updates Door and Footswitch state are sent at program start to make sure c# is in sync. After that, we only send on state change.

--- AutoGrind/Properties/AssemblyInfo.cs
+++ AutoGrind/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2022.05.19.07")]
-[assembly: AssemblyFileVersion("2022.05.19.07")]
+[assembly: AssemblyVersion("2022.05.20.12")]
+[assembly: AssemblyFileVersion("2022.05.20.12")]
