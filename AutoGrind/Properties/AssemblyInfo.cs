1da6f0467bd9be5b419b5f00f60308c0233a19ae
Ned Lecky <ned@lecky.com>
2022-07-01 10:27:12 -0500

    Update revision to today

--- AutoGrind/Properties/AssemblyInfo.cs
+++ AutoGrind/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2022.06.30.15")]
-[assembly: AssemblyFileVersion("2022.06.30.15")]
+[assembly: AssemblyVersion("2022.07.01.10")]
+[assembly: AssemblyFileVersion("2022.07.01.10")]
